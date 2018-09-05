78932d5c54f28d3accedd2e3dd9408367abde2ba
YanC Lin <yonalin1@gmail.com>
2018-09-05 20:11:35 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -10,3 +10,11 @@ function lastImageSelector() {
 function ninjaBabySelector() {
   return $('#baby-ninja')
 }
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('#pic-list ul:first-child')
+}
